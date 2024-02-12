f036f4a86f970bf3f88dbbbe02c35ae250ae373e
AzharKhanPlay <azharkhanplay@gmail.com>
2024-02-13 00:21:16 +0530

    Serverless and lambda packages added

--- src/api.js
+++ src/api.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const winston = require("winston");
+const serverless = require("serverless-http");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const router = express.Router();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -13,6 +15,12 @@ const logger = winston.createLogger({
     transports: [
         new winston.transports.Console(),
     ]
+});
+
+router.get('/', (req, res) => {
+  res.json({
+    hello: 'Hi'
+  })
 })
 
 app.post('/webhook', function(req, res){
@@ -22,7 +30,13 @@ app.post('/webhook', function(req, res){
    res.status(200).send('Webhook received');
 });
 
+
+app.use(`/.netlify/functions/api`, router);
+
 app.listen(port, () => { 
     console.log('listening on port'+ port); 
     logger.info('listening on port'+ port); 
 });
+
+module.exports = app;
+module.exports.handler = serverless(app);
